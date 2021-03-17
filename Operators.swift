44957344a3670bb02871089fa93f47a836925bf4
Sean LaFlam <sean.laflam@gmail.com>
2021-03-17 00:10:33 -0400

    Feeling Well

---
+++ Operators.swift
@@ -0,0 +1,14 @@
+var carInMotion = false
+var insideACar = true
+
+if carInMotion || insideACar {
+  print("Safety first! Buckle up.")
+}
+
+var feelingWell = true
+
+if !feelingWell {
+  print("Have some vitamins and take care of yourself 🤒")
+} else {
+  print("Embrace the day!")
+}
--- SafetyFirst.swift
+++
@@ -1,6 +0,0 @@
-var carInMotion = false
-var insideACar = true
-
-if carInMotion || insideACar {
-  print("Safety first! Buckle up.")
-}
