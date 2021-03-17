325822ed91a36d8af7862609aba9ce4bc7ac138e
Sean LaFlam <sean.laflam@gmail.com>
2021-03-17 00:25:00 -0400

    Eightball

--- 8Ball.swift
+++ 8Ball.swift
@@ -2,3 +2,31 @@
 
 // In this project, you’ll hone your conditional skills by creating a program that can answer any “Yes” or “No” question with a different fortune each time.
 
+var playerName = "Sean"
+var playerQuestion = "Will I get a job?"
+var randomNumber = Int.random(in: 1...9)
+var eightBall:String
+
+switch randomNumber {
+    case 1:
+        eightBall = "Yes - definitely"
+    case 2:
+        eightBall = "It is decidedly so"
+    case 3:
+        eightBall = "Without a doubt"
+    case 4:
+        eightBall = "Reply hazy, try again"
+    case 5:
+        eightBall = "Ask again later"
+    case 6:
+        eightBall = "Better not tell you now"
+    case 7:
+        eightBall = "My sources say no"
+    case 8:
+        eightBall = "Outlook not so good"
+    case 9:
+        eightBall = "Very doubtful"
+    default:
+        eightBall = "Error"
+}
+
