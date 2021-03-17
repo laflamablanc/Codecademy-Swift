1bf0d01902c7e9e4820a4b6fe14d3f2b8a985c80
Sean LaFlam <sean.laflam@gmail.com>
2021-03-17 00:18:43 -0400

    Initial

---
+++ 8Ball.swift
@@ -0,0 +1,4 @@
+// The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
+
+// In this project, you’ll hone your conditional skills by creating a program that can answer any “Yes” or “No” question with a different fortune each time.
+
--- Operators.swift
+++ Operators.swift
@@ -12,3 +12,17 @@ if !feelingWell {
 } else {
   print("Embrace the day!")
 }
+
+var correctPassword = false
+var lessThanThreeTries = true 
+var accessThroughTouchID = true
+var unlock: Bool
+
+if correctPassword && lessThanThreeTries || accessThroughTouchID {
+  unlock = true
+} else {
+  unlock = false
+}
+
+print(unlock)
+
